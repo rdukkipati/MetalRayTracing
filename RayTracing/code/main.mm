84f9ffa5cbcd864ac46c69742dea82aec89d4a4f
Raghav Dukkipati <85905784+rdukkipati@users.noreply.github.com>
2026-06-20 12:19:34 -0400

    mook

--- RayTracing/code/main.mm
+++ RayTracing/code/main.mm
@@ -1,52 +1,69 @@
-#include <Metal/Metal.h>
 #include <Foundation/Foundation.h>
+#include <Metal/Metal.h>
 #include <limits.h>
 #include <mach-o/dyld.h>
 
-#include <stdlib.h>
+#include <simd/simd.h>
+
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define internal static
-#define local_persist static
+#define internal        static
+#define local_persist   static
 #define global_variable static
 
-typedef int8_t i8;
+typedef int8_t  i8;
 typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
-typedef i32 b32;
+typedef i32     b32;
 
-#define true 1
+#define true  1
 #define false 0
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+typedef uint8_t     u8;
+typedef uint16_t    u16;
+typedef uint32_t    u32;
+typedef uint64_t    u64;
 
-typedef float f32;
-typedef double f64;
+typedef float       f32;
+typedef double      f64;
 
 global_variable i32 Image_Height = 2234;
-global_variable i32 Image_Width = 3456;
+global_variable i32 Image_Width  = 3456;
+
+struct sphere
+{
+	simd_float1 Radius;
+	simd_float3 Center;
+};
+
+internal sphere
+_Sphere(simd_float1 Radius, simd_float3 Center)
+{
+	sphere Result;
+	Result.Radius = Radius;
+	Result.Center = Center;
+	return Result;
+}
 
 struct state
 {
-    char ExecutablePath[PATH_MAX];
+    char  ExecutablePath[PATH_MAX];
     char *ExecutableDirectory;
 };
 
 internal void
 GetExecutablePath(state *State)
 {
-    char *ExecutablePath = State->ExecutablePath;
+    char *ExecutablePath      = State->ExecutablePath;
     char *ExecutableDirectory = State->ExecutableDirectory;
-    u32 Size = sizeof(State->ExecutablePath);
-    
+    u32   Size                = sizeof(State->ExecutablePath);
+
     _NSGetExecutablePath(ExecutablePath, &Size);
     ExecutableDirectory = ExecutablePath;
-    
+
     for(char *Scan = ExecutableDirectory; *Scan; ++Scan)
     {
         if(*Scan == '/')
@@ -54,17 +71,16 @@ GetExecutablePath(state *State)
             ExecutableDirectory = Scan + 1;
         }
     }
-    
+
     State->ExecutableDirectory = ExecutableDirectory;
-    
 }
 
 internal void
 BuildFullPath(state *State, char *Filename, size_t Size, char *FullPath)
 {
-    char *ExecutablePath = State->ExecutablePath;
+    char *ExecutablePath      = State->ExecutablePath;
     char *ExecutableDirectory = State->ExecutableDirectory;
-    int DirectoryLength = ExecutableDirectory - ExecutablePath;
+    int   DirectoryLength     = ExecutableDirectory - ExecutablePath;
     for(int Index = 0; Index < DirectoryLength; ++Index)
     {
         *FullPath++ = *ExecutablePath++;
@@ -76,109 +92,156 @@ BuildFullPath(state *State, char *Filename, size_t Size, char *FullPath)
     *FullPath = 0;
 }
 
-i32 main(i32 argc, const char *argv[]) {
-    
+i32
+main(i32 argc, const char *argv[])
+{
+
     @autoreleasepool
     {
         state State;
         GetExecutablePath(&State);
-        
-        NSString *NSExecutablePath = [NSString stringWithUTF8String:State.ExecutablePath];
-        NSString *NSExecutableDirectory = [NSString stringWithUTF8String:State.ExecutableDirectory];
-        
-        char MetalLibraryFilename[] = "shaders.metallib";
-        char MetalLibraryFullPath[PATH_MAX];
-        
-        BuildFullPath(&State, MetalLibraryFilename, sizeof(MetalLibraryFilename), MetalLibraryFullPath);
-        
-        NSString *NSString_MetalLibraryFullPath = [NSString stringWithUTF8String:MetalLibraryFullPath];
-        NSURL *NSURL_MetalLibraryFullPath = [NSURL fileURLWithPath:NSString_MetalLibraryFullPath];
-        
-        id<MTLDevice> Device = MTLCreateSystemDefaultDevice();
-        
-        NSError *Errors = nil;
-        id<MTLLibrary> MetalLibrary = [Device newLibraryWithURL:NSURL_MetalLibraryFullPath error:&Errors];
+
+        NSString *NSExecutablePath       = [NSString
+            stringWithUTF8String:State.ExecutablePath];
+        NSString *NSExecutableDirectory  = [NSString
+            stringWithUTF8String:State.ExecutableDirectory];
+
+        char      MetalLibraryFilename[] = "shaders.metallib";
+        char      MetalLibraryFullPath[PATH_MAX];
+
+        BuildFullPath(&State, MetalLibraryFilename,
+                      sizeof(MetalLibraryFilename), MetalLibraryFullPath);
+
+        NSString      *NSString_MetalLibraryFullPath = [NSString
+            stringWithUTF8String:MetalLibraryFullPath];
+        NSURL         *NSURL_MetalLibraryFullPath    = [NSURL
+            fileURLWithPath:NSString_MetalLibraryFullPath];
+
+        id<MTLDevice>  Device       = MTLCreateSystemDefaultDevice();
+
+        NSError       *Errors       = nil;
+        id<MTLLibrary> MetalLibrary = [Device
+            newLibraryWithURL:NSURL_MetalLibraryFullPath
+                        error:&Errors];
         if(!MetalLibrary)
         {
             NSLog(@"Library load failed: %@", Errors);
             return 1;
         }
-        id<MTLFunction> VertexFunction = [MetalLibrary newFunctionWithName:@"VertexFunction"];
-        id<MTLFunction> FragmentFunction = [MetalLibrary newFunctionWithName:@"FragmentFunction"];
-        
-        id<MTLCommandQueue> CommandQueue = [Device newCommandQueueWithMaxCommandBufferCount:64];
-        
-        MTLTextureDescriptor *TextureDescriptor = [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormatRGBA8Unorm width:Image_Width height:Image_Height mipmapped:NO];
-        
-        id<MTLTexture> Texture = [Device newTextureWithDescriptor:TextureDescriptor];
-        
-        MTLRenderPassDescriptor *RenderPassDescriptor = [MTLRenderPassDescriptor renderPassDescriptor];
-        
+        id<MTLFunction>          VertexFunction    = [MetalLibrary
+            newFunctionWithName:@"VertexFunction"];
+        id<MTLFunction>          FragmentFunction  = [MetalLibrary
+            newFunctionWithName:@"FragmentFunction"];
+
+        id<MTLCommandQueue>      CommandQueue      = [Device
+            newCommandQueueWithMaxCommandBufferCount:64];
+
+        MTLTextureDescriptor    *TextureDescriptor = [MTLTextureDescriptor
+            texture2DDescriptorWithPixelFormat:MTLPixelFormatRGBA8Unorm
+                                         width:Image_Width
+                                        height:Image_Height
+                                     mipmapped:NO];
+
+        id<MTLTexture>           Texture           = [Device
+            newTextureWithDescriptor:TextureDescriptor];
+
+        MTLRenderPassDescriptor *RenderPassDescriptor =
+            [MTLRenderPassDescriptor renderPassDescriptor];
+
         RenderPassDescriptor.colorAttachments[0].texture = Texture;
-        RenderPassDescriptor.colorAttachments[0].loadAction = MTLLoadActionDontCare;
-        RenderPassDescriptor.colorAttachments[0].storeAction = MTLStoreActionStore;
-        
+        RenderPassDescriptor.colorAttachments[0].loadAction =
+            MTLLoadActionDontCare;
+        RenderPassDescriptor.colorAttachments[0].storeAction =
+            MTLStoreActionStore;
+
         id<MTLCommandBuffer> CommandBuffer = [CommandQueue commandBuffer];
-        
-        id<MTLRenderCommandEncoder> RenderCommandEncoder = [CommandBuffer renderCommandEncoderWithDescriptor:RenderPassDescriptor];
-        
-        MTLRenderPipelineDescriptor *RenderPipelineDescriptor = [[MTLRenderPipelineDescriptor alloc] init];
-        
-        RenderPipelineDescriptor.vertexFunction = VertexFunction;
+
+        id<MTLRenderCommandEncoder>  RenderCommandEncoder = [CommandBuffer
+            renderCommandEncoderWithDescriptor:RenderPassDescriptor];
+
+        MTLRenderPipelineDescriptor *RenderPipelineDescriptor =
+            [[MTLRenderPipelineDescriptor alloc] init];
+
+        RenderPipelineDescriptor.vertexFunction   = VertexFunction;
         RenderPipelineDescriptor.fragmentFunction = FragmentFunction;
-        RenderPipelineDescriptor.colorAttachments[0].pixelFormat = MTLPixelFormatRGBA8Unorm;
-        
-        MTLVertexDescriptor *VertexDescriptor = [[MTLVertexDescriptor alloc] init];
-        
+        RenderPipelineDescriptor.colorAttachments[0].pixelFormat =
+            MTLPixelFormatRGBA8Unorm;
+
+        MTLVertexDescriptor *VertexDescriptor = [[MTLVertexDescriptor alloc]
+            init];
+
         VertexDescriptor.attributes[0].format = MTLVertexFormatFloat2;
         VertexDescriptor.attributes[0].bufferIndex = 0;
-        VertexDescriptor.attributes[0].offset = 0;
-        VertexDescriptor.attributes[1].format = MTLVertexFormatFloat2;
+        VertexDescriptor.attributes[0].offset      = 0;
+        VertexDescriptor.attributes[1].format      = MTLVertexFormatFloat2;
         VertexDescriptor.attributes[1].bufferIndex = 0;
-        VertexDescriptor.attributes[1].offset = 2 * sizeof(float);
-        VertexDescriptor.layouts[0].stride = 4 * sizeof(float);
-        VertexDescriptor.layouts[0].stepFunction = MTLVertexStepFunctionPerVertex;
-        
-        RenderPipelineDescriptor.vertexDescriptor = VertexDescriptor;
-        
-        Errors = nil;
-        
-        id<MTLRenderPipelineState> RenderPipelineState = [Device newRenderPipelineStateWithDescriptor:RenderPipelineDescriptor error:&Errors];
-        
+        VertexDescriptor.attributes[1].offset      = 2 * sizeof(float);
+        VertexDescriptor.layouts[0].stride         = 4 * sizeof(float);
+        VertexDescriptor.layouts[0].stepFunction =
+            MTLVertexStepFunctionPerVertex;
+
+        RenderPipelineDescriptor.vertexDescriptor      = VertexDescriptor;
+
+        Errors                                         = nil;
+
+        id<MTLRenderPipelineState> RenderPipelineState = [Device
+            newRenderPipelineStateWithDescriptor:RenderPipelineDescriptor
+                                           error:&Errors];
+
         [RenderCommandEncoder setRenderPipelineState:RenderPipelineState];
-        
-        float VerticesAndUVs[] = { 
-            -1.0f, -1.0f, 0.0f, 1.0f, 
-            -1.0f, 1.0f,  0.0f, 0.0f,
-            1.0f,  -1.0f, 1.0f, 1.0f, 
-            1.0f,  -1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f,  0.0f, 0.0f, 
-            1.0f,  1.0f,  1.0f, 0.0f,
+
+        float VerticesAndUVs[] = {
+            -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 1.0f,  0.0f, 0.0f,
+            1.0f,  -1.0f, 1.0f, 1.0f, 1.0f,  -1.0f, 1.0f, 1.0f,
+            -1.0f, 1.0f,  0.0f, 0.0f, 1.0f,  1.0f,  1.0f, 0.0f,
         };
-        
-        id<MTLBuffer> VertexBuffer = [Device newBufferWithBytes:VerticesAndUVs length:sizeof(VerticesAndUVs) options:nil];
-        
+
+        id<MTLBuffer> VertexBuffer = [Device
+            newBufferWithBytes:VerticesAndUVs
+                        length:sizeof(VerticesAndUVs)
+                       options:0];
+
         [RenderCommandEncoder setVertexBuffer:VertexBuffer offset:0 atIndex:0];
-        
-        [RenderCommandEncoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:6];
-        
+
+		sphere Spheres[10];
+		Spheres[0] = _Sphere(0.5f, simd_make_float3(0, 0, -1));
+		Spheres[1] = _Sphere(100.0f, simd_make_float3(0, -100.5f, -1));
+
+		id<MTLBuffer> SphereBuffer = [Device newBufferWithBytes:Spheres length:sizeof(Spheres) options:0];
+
+		[RenderCommandEncoder setFragmentBuffer:SphereBuffer offset:0 atIndex:1];
+
+		u32 SphereCount = 2;
+
+		id<MTLBuffer> SphereCountBuffer = [Device newBufferWithBytes:&SphereCount length:sizeof(SphereCount) options:0];
+
+		[RenderCommandEncoder setFragmentBuffer:SphereCountBuffer offset:0 atIndex:2];
+
+        [RenderCommandEncoder drawPrimitives:MTLPrimitiveTypeTriangle
+                                 vertexStart:0
+                                 vertexCount:6];
+
         [RenderCommandEncoder endEncoding];
         [CommandBuffer commit];
-        
-        [CommandBuffer waitUntilCompleted]; //Image_Width Image_Height
-        
+
+        [CommandBuffer waitUntilCompleted]; // Image_Width Image_Height
+
         u8 *Pixels = (u8 *)malloc(Image_Height * (Image_Width * 4));
-        
-        [Texture getBytes:Pixels bytesPerRow:Image_Width*4 fromRegion:MTLRegionMake2D(0, 0, Image_Width, Image_Height) mipmapLevel:0];
-        
+
+        [Texture getBytes:Pixels
+              bytesPerRow:Image_Width * 4
+               fromRegion:MTLRegionMake2D(0, 0, Image_Width, Image_Height)
+              mipmapLevel:0];
+
         char OutputFilename[] = "output.ppm";
         char OutputFullPath[PATH_MAX];
-        
-        BuildFullPath(&State, OutputFilename, sizeof(OutputFilename), OutputFullPath);
-        
+
+        BuildFullPath(&State, OutputFilename, sizeof(OutputFilename),
+                      OutputFullPath);
+
         FILE *File = fopen(OutputFullPath, "wb");
         fprintf(File, "P6\n%d %d\n255\n", Image_Width, Image_Height);
-        
+
         for(int Row = 0; Row < Image_Height; ++Row)
         {
             for(int Col = 0; Col < Image_Width; ++Col)
@@ -187,10 +250,9 @@ i32 main(i32 argc, const char *argv[]) {
                 fwrite(Pixel, 1, 3, File);
             }
         }
-        
+
         fclose(File);
-        
+
         printf("ray tracing done\n");
-        
     }
 }
--- RayTracing/code/shaders.metal
+++ RayTracing/code/shaders.metal
@@ -1,116 +1,198 @@
-#include <metal_common>
-#include <metal_geometric>
+#include <metal_stdlib>
 
 using namespace metal;
 
 #define internal static
+#define PI       3.14159265359f
 
-typedef float3 vec3;
-typedef float3 point3;
-typedef float3 color3;
+typedef float3            vec3;
+typedef float3            point3;
+typedef float3            color3;
 
-typedef int b32;
+constant constexpr int    Image_Height   = 2234;
+constant constexpr int    Image_Width    = 3456;
+constant constexpr float  FocalLength    = 1.0f;
+constant constexpr float  ViewportHeight = 2.0f;
+constant constexpr float  ViewportWidth  = ViewportHeight *
+                                           ((float)Image_Width / Image_Height);
+constant constexpr point3 CameraCenter   = point3(0, 0, 0);
 
-constant constexpr int Image_Height = 2234;
-constant constexpr int Image_Width = 3456;
-constant constexpr float FocalLength = 1.0f;
-constant constexpr float ViewportHeight = 2.0f;
-constant constexpr float ViewportWidth = ViewportHeight * ((float)Image_Width / Image_Height);
-constant constexpr point3 CameraCenter = point3(0, 0, 0);
+constant constexpr vec3   Viewport_U     = vec3(ViewportWidth, 0, 0);
+constant constexpr vec3   Viewport_V     = vec3(0, -ViewportHeight, 0);
 
-constant constexpr vec3 Viewport_U = vec3(ViewportWidth, 0, 0);
-constant constexpr vec3 Viewport_V = vec3(0, -ViewportHeight, 0);
+constant constexpr point3 ViewportUpperLeft = CameraCenter -
+                                              vec3(0, 0, FocalLength) -
+                                              (Viewport_U / 2) -
+                                              (Viewport_V / 2);
 
-constant constexpr point3 ViewportUpperLeft = CameraCenter - vec3(0, 0, FocalLength) - (Viewport_U / 2) - (Viewport_V / 2);
+internal float
+DegreesToRadians(float Degrees)
+{
+	return Degrees * PI / 180.0f;
+}
 
 struct ray
 {
-	point3 Origin;
-	vec3 Direction;
+    point3 Origin;
+    vec3   Direction;
 };
 
 internal ray
 _Ray(point3 Origin, vec3 Direction)
 {
-	ray Result;
-	Result.Origin = Origin;
-	Result.Direction = Direction;
-	return Result;
+    ray Result;
+    Result.Origin    = Origin;
+    Result.Direction = Direction;
+    return Result;
 }
 
 internal point3
 RayAt(ray Ray, float Time)
 {
-	return Ray.Origin + (Time * Ray.Direction);
+    return Ray.Origin + (Time * Ray.Direction);
+}
+
+struct sphere
+{
+    float  Radius;
+    point3 Center;
+};
+
+struct world
+{
+	constant sphere *Spheres;
+	uint SphereCount;
+};
+
+struct hit
+{
+    point3 Point;
+    vec3   Normal;
+    float  Time;
+	bool Outside;
+};
+
+internal void
+SetFaceNormal(ray Ray, vec3 OutwardNormal, thread hit *Hit)
+{
+	Hit->Outside = dot(Ray.Direction, OutwardNormal) < 0;
+	Hit->Normal = Hit->Outside ? OutwardNormal : -OutwardNormal;
+}
+
+internal bool
+HitSphere(sphere Sphere, ray Ray, float Time_Min, float Time_Max, thread hit *Hit)
+{
+	float Radius = Sphere.Radius;
+    vec3  OriginToCenter = Sphere.Center - Ray.Origin;
+    float a              = dot(Ray.Direction, Ray.Direction);
+    float h              = dot(Ray.Direction, OriginToCenter);
+    float c            = dot(OriginToCenter, OriginToCenter) - Radius * Radius;
+    float Discriminant = h * h - a * c;
+
+    if(Discriminant < 0)
+    {
+        return false;
+    }
+	float Root = sqrt(Discriminant);
+	float Time = (h - Root) / a;
+	if(Time <= Time_Min || Time >= Time_Max)
+	{
+		Time = (h + Root) / a;
+		if(Time <= Time_Min || Time >= Time_Max)
+		{
+			return false;
+		}
+	}
+	Hit->Time = Time;
+	Hit->Point = RayAt(Ray, Time);
+	vec3 OutwardNormal = (Hit->Point - Sphere.Center) / Sphere.Radius;
+	SetFaceNormal(Ray, OutwardNormal, Hit);
+	
+	return true;
+}
+
+internal bool
+HitWorld(world World, ray Ray, float Time_Min, float Time_Max, thread hit *Hit)
+{
+	bool HitAnything = false;
+	float ClosestTime = Time_Max;
+
+	for(uint Count = 0; Count < World.SphereCount; ++Count)
+	{
+		sphere Sphere = World.Spheres[Count];
+		if(HitSphere(Sphere, Ray, Time_Min, ClosestTime, Hit))
+		{
+			HitAnything = true;
+			ClosestTime = Hit->Time;
+		}
+	}
+
+	return HitAnything;
+}
+
+internal color3
+RayColor(ray Ray, world World)
+{
+	hit Hit;
+	if(HitWorld(World, Ray, 0, INFINITY, &Hit))
+    {
+		return 0.5f * (Hit.Normal + color3(1, 1, 1));
+    }
+
+    vec3   UnitDirection = normalize(Ray.Direction);
+    float  LerpFactor    = 0.5f * (UnitDirection.y + 1.0f);
+    color3 SkyColor      = mix(float3(1.0, 1.0, 1.0), float3(0.5, 0.7, 1.0),
+                               LerpFactor);
+    return SkyColor;
 }
 
 struct vertex_in
 {
-	float2 Position [[attribute(0)]];
-	float2 UV [[attribute(1)]];
+    float2 Position [[attribute(0)]];
+    float2 UV [[attribute(1)]];
 };
 
 struct vertex_out
 {
-	float4 Position [[position]];
-	float2 UV;
+    float4 Position [[position]];
+    float2 UV;
 };
 
 struct fragment_out
 {
-	half4 Color;
+    half4 Color;
 };
-
-internal bool
-HitSphere(point3 Center, float Radius, ray Ray)
-{
-	vec3 OriginToCenter = Center - Ray.Origin;
-	float a = dot(Ray.Direction, Ray.Direction);
-	float b = -2.0f * dot(Ray.Direction, OriginToCenter);
-	float c = dot(OriginToCenter, OriginToCenter) - Radius*Radius;
-	float Discriminant = b*b - 4*a*c;
-	return Discriminant >= 0;
-}
-
-internal float3
-RayColor(ray Ray)
-{
-	bool Hit = HitSphere(point3(0, 0, -1), 0.5f, Ray);
-	
-	vec3 UnitDirection = normalize(Ray.Direction);
-	float LerpFactor = 0.5f * (UnitDirection.y + 1.0f);
-	float3 SkyColor = mix(float3(1.0, 1.0, 1.0), 
-						  float3(0.5, 0.7, 1.0), 
-						  LerpFactor);
-
-	return mix(SkyColor, float3(1.0, 0.0, 0.0), Hit);
-}
 
 [[vertex]] vertex_out
 VertexFunction(vertex_in Vertex [[stage_in]])
 {
-	vertex_out Result;
-	
-	Result.Position = float4(Vertex.Position, 0.0f, 1.0f);
-	Result.UV = Vertex.UV;
+    vertex_out Result;
 
-	return Result;
+    Result.Position = float4(Vertex.Position, 0.0f, 1.0f);
+    Result.UV       = Vertex.UV;
+
+    return Result;
 }
 
 [[fragment]] fragment_out
-FragmentFunction(vertex_out Fragment [[stage_in]])
+FragmentFunction(vertex_out       Fragment [[stage_in]],
+                 constant sphere *Spheres [[buffer(1)]],
+                 constant uint   *SphereCount [[buffer(2)]])
 {
-	fragment_out Result;
+	world World = { Spheres, *SphereCount };
 
-	point3 PixelCenterInWorldSpace = ViewportUpperLeft + 
-									 Fragment.UV.x * Viewport_U + 
-									 Fragment.UV.y * Viewport_V;
 
-	vec3 RayDirection = PixelCenterInWorldSpace - CameraCenter;
-	ray Ray = _Ray(CameraCenter, RayDirection);
- 
-	float3 PixelColor = RayColor(Ray);
-	Result.Color = half4(half3(PixelColor), 1.0);
+    fragment_out Result;
 
-	return Result;
+    point3       PixelCenterInWorldSpace = ViewportUpperLeft +
+                                           Fragment.UV.x * Viewport_U +
+                                           Fragment.UV.y * Viewport_V;
+
+    vec3         RayDirection = PixelCenterInWorldSpace - CameraCenter;
+    ray          Ray          = _Ray(CameraCenter, RayDirection);
+
+    color3       PixelColor   = RayColor(Ray, World);
+    Result.Color              = half4(half3(PixelColor), 1.0);
+
+    return Result;
 }
