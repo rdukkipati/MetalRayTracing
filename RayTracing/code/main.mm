1caf7a30d7f7c0658e13f36d74d76090fdd8c44c
Raghav Dukkipati <85905784+rdukkipati@users.noreply.github.com>
2026-07-02 00:42:27 -0500

    ..

--- RayTracing/code/main.mm
+++ RayTracing/code/main.mm
@@ -163,36 +163,36 @@ _Camera(camera *Camera)
 {
     Camera->ImageHeight       = IMAGE_HEIGHT;
     Camera->ImageWidth        = IMAGE_WIDTH;
-
+    
     Camera->SamplesPerPixel   = SAMPLES_PER_PIXEL;
     Camera->PixelSamplesScale = 1.0f / Camera->SamplesPerPixel;
-
+    
     Camera->MaxRayBounces     = MAX_RAY_BOUNCES;
-
+    
     f32 Theta                 = DegreesToRadians(VERTICAL_FIELD_OF_VIEW);
     f32 h                     = Tan_f32(Theta / 2);
     f32 ViewportHeight        = 2 * h * FOCUS_DISTANCE;
     f32 ViewportWidth    = ViewportHeight * ((f32)IMAGE_WIDTH / IMAGE_HEIGHT);
-
+    
     Camera->Center       = LOOK_FROM;
-
+    
     vec3 Back            = normalize(LOOK_FROM - LOOK_AT);
     vec3 Right           = normalize(cross(WORLD_UP, Back));
     vec3 Up              = cross(Back, Right);
-
+    
     vec3 Viewport_U      = ViewportWidth * Right;
     vec3 Viewport_V      = -ViewportHeight * Up;
-
+    
     Camera->PixelDelta_U = Viewport_U / Camera->ImageWidth;
     Camera->PixelDelta_V = Viewport_V / Camera->ImageHeight;
-
+    
     Camera->ViewportUpperLeft = Camera->Center - (FOCUS_DISTANCE * Back) -
-                                (Viewport_U / 2) - (Viewport_V / 2);
-
+        (Viewport_U / 2) - (Viewport_V / 2);
+    
     Camera->DefocusAngle      = DEFOCUS_ANGLE;
-
+    
     f32 DefocusRadius         = FOCUS_DISTANCE *
-                                Tan_f32(DegreesToRadians(DEFOCUS_ANGLE / 2));
+        Tan_f32(DegreesToRadians(DEFOCUS_ANGLE / 2));
     Camera->DefocusDisk_U     = Right * DefocusRadius;
     Camera->DefocusDisk_V     = Up * DefocusRadius;
 }
@@ -284,10 +284,10 @@ GetExecutablePath(state *State)
     char *ExecutablePath      = State->ExecutablePath;
     char *ExecutableDirectory = State->ExecutableDirectory;
     u32   Size                = sizeof(State->ExecutablePath);
-
+    
     _NSGetExecutablePath(ExecutablePath, &Size);
     ExecutableDirectory = ExecutablePath;
-
+    
     for(char *Scan = ExecutableDirectory; *Scan; ++Scan)
     {
         if(*Scan == '/')
@@ -295,7 +295,7 @@ GetExecutablePath(state *State)
             ExecutableDirectory = Scan + 1;
         }
     }
-
+    
     State->ExecutableDirectory = ExecutableDirectory;
 }
 
@@ -319,78 +319,78 @@ BuildFullPath(state *State, char *Filename, size_t Size, char *FullPath)
 i32
 main(i32 argc, const char *argv[])
 {
-
+    
     @autoreleasepool
     {
         state State;
         GetExecutablePath(&State);
-
+        
         NSString *NSExecutablePath       = [NSString
-            stringWithUTF8String:State.ExecutablePath];
+                                            stringWithUTF8String:State.ExecutablePath];
         NSString *NSExecutableDirectory  = [NSString
-            stringWithUTF8String:State.ExecutableDirectory];
-
+                                            stringWithUTF8String:State.ExecutableDirectory];
+        
         char      MetalLibraryFilename[] = "shaders.metallib";
         char      MetalLibraryFullPath[PATH_MAX];
-
+        
         BuildFullPath(&State, MetalLibraryFilename,
                       sizeof(MetalLibraryFilename), MetalLibraryFullPath);
-
+        
         NSString      *NSString_MetalLibraryFullPath = [NSString
-            stringWithUTF8String:MetalLibraryFullPath];
+                                                        stringWithUTF8String:MetalLibraryFullPath];
         NSURL         *NSURL_MetalLibraryFullPath    = [NSURL
-            fileURLWithPath:NSString_MetalLibraryFullPath];
-
+                                                        fileURLWithPath:NSString_MetalLibraryFullPath];
+        
         id<MTLDevice>  Device       = MTLCreateSystemDefaultDevice();
-
+        
         NSError       *Errors       = nil;
         id<MTLLibrary> MetalLibrary = [Device
-            newLibraryWithURL:NSURL_MetalLibraryFullPath
-                        error:&Errors];
+                                       newLibraryWithURL:NSURL_MetalLibraryFullPath
+                                       error:&Errors];
         if(!MetalLibrary)
         {
             NSLog(@"Library load failed: %@", Errors);
             return 1;
         }
         id<MTLFunction>          VertexFunction    = [MetalLibrary
-            newFunctionWithName:@"VertexFunction"];
+                                                      newFunctionWithName:@"VertexFunction"];
         id<MTLFunction>          FragmentFunction  = [MetalLibrary
-            newFunctionWithName:@"FragmentFunction"];
-
+                                                      newFunctionWithName:@"FragmentFunction"];
+        
         // Note: for game rendering loop, will need to limit # of frames in
         // flight so that command queue does not overflow
         id<MTLCommandQueue>      CommandQueue      = [Device
-            newCommandQueueWithMaxCommandBufferCount:64];
-
+                                                      newCommandQueueWithMaxCommandBufferCount:64];
+        
         MTLTextureDescriptor    *TextureDescriptor = [MTLTextureDescriptor
-            texture2DDescriptorWithPixelFormat:MTLPixelFormatRGBA8Unorm_sRGB
-                                         width:IMAGE_WIDTH
-                                        height:IMAGE_HEIGHT
-                                     mipmapped:NO];
-
+                                                      texture2DDescriptorWithPixelFormat:MTLPixelFormatRGBA8Unorm_sRGB
+                                                      width:IMAGE_WIDTH
+                                                      height:IMAGE_HEIGHT
+                                                      mipmapped:NO];
+        
         // Note: textures expensive to create
         id<MTLTexture>           Texture           = [Device
-            newTextureWithDescriptor:TextureDescriptor];
-
+                                                      newTextureWithDescriptor:TextureDescriptor];
+        
         MTLRenderPassDescriptor *RenderPassDescriptor =
             [MTLRenderPassDescriptor renderPassDescriptor];
-
+        
         RenderPassDescriptor.colorAttachments[0].texture    = Texture;
         RenderPassDescriptor.colorAttachments[0].loadAction = MTLLoadActionLoad;
         RenderPassDescriptor.colorAttachments[0].storeAction =
             MTLStoreActionStore;
-
+        
         MTLRenderPipelineDescriptor *RenderPipelineDescriptor =
             [[MTLRenderPipelineDescriptor alloc] init];
-
+        
         RenderPipelineDescriptor.vertexFunction   = VertexFunction;
         RenderPipelineDescriptor.fragmentFunction = FragmentFunction;
         RenderPipelineDescriptor.colorAttachments[0].pixelFormat =
             MTLPixelFormatRGBA8Unorm_sRGB;
-
+        
         MTLVertexDescriptor *VertexDescriptor = [[MTLVertexDescriptor alloc]
-            init];
-
+                                                 init];
+        
         VertexDescriptor.attributes[0].format = MTLVertexFormatFloat2;
         VertexDescriptor.attributes[0].bufferIndex = 0;
         VertexDescriptor.attributes[0].offset      = 0;
@@ -400,29 +400,29 @@ main(i32 argc, const char *argv[])
         VertexDescriptor.layouts[0].stride         = 4 * sizeof(float);
         VertexDescriptor.layouts[0].stepFunction =
             MTLVertexStepFunctionPerVertex;
-
+        
         RenderPipelineDescriptor.vertexDescriptor      = VertexDescriptor;
-
+        
         Errors                                         = nil;
-
+        
         id<MTLRenderPipelineState> RenderPipelineState = [Device
-            newRenderPipelineStateWithDescriptor:RenderPipelineDescriptor
-                                           error:&Errors];
-
+                                                          newRenderPipelineStateWithDescriptor:RenderPipelineDescriptor
+                                                          error:&Errors];
+        
         float                      VerticesAndUVs[]    = {
             -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 1.0f,  0.0f, 0.0f,
             1.0f,  -1.0f, 1.0f, 1.0f, 1.0f,  -1.0f, 1.0f, 1.0f,
             -1.0f, 1.0f,  0.0f, 0.0f, 1.0f,  1.0f,  1.0f, 0.0f,
         };
-
+        
         // Note: Buffers expensive to create
         id<MTLBuffer> VertexBuffer = [Device
-            newBufferWithBytes:VerticesAndUVs
-                        length:sizeof(VerticesAndUVs)
-                       options:0];
-
+                                      newBufferWithBytes:VerticesAndUVs
+                                      length:sizeof(VerticesAndUVs)
+                                      options:0];
+        
         material      Ground       = _Lambertian(_Color3(0.5f, 0.5f, 0.5f));
-
+        
         rng           RNG;
         InitializeRNG(&RNG, 1223, 832);
         world World = {};
@@ -460,48 +460,48 @@ main(i32 argc, const char *argv[])
         }
         material Material1 = _Dielectric(1.5f);
         WorldAdd(&World, _Sphere(1.0f, _Point3(0, 1, 0), Material1));
-
+        
         material Material2 = _Lambertian(_Color3(0.4f, 0.2f, 0.1f));
         WorldAdd(&World, _Sphere(1.0f, _Point3(-4, 1, 0), Material2));
-
+        
         material Material3 = _Metal(_Color3(0.7f, 0.6f, 0.5f), 0);
         WorldAdd(&World, _Sphere(1.0f, _Point3(4, 1, 0), Material3));
-
+        
         // Note: Buffers expensive to create
         id<MTLBuffer> SphereBuffer      = [Device
-            newBufferWithBytes:World.Spheres
-                        length:sizeof(World.Spheres)
-                       options:0];
-
+                                           newBufferWithBytes:World.Spheres
+                                           length:sizeof(World.Spheres)
+                                           options:0];
+        
         // Note: Buffers expensive to create
         id<MTLBuffer> SphereCountBuffer = [Device
-            newBufferWithBytes:&World.Count
-                        length:sizeof(World.Count)
-                       options:0];
-
+                                           newBufferWithBytes:&World.Count
+                                           length:sizeof(World.Count)
+                                           options:0];
+        
         camera        Camera            = {};
         _Camera(&Camera);
         id<MTLBuffer> CameraBuffer = [Device newBufferWithBytes:&Camera
-                                                         length:sizeof(Camera)
-                                                        options:0];
+                                      length:sizeof(Camera)
+                                      options:0];
         id<MTLCommandBuffer> CommandBuffer = [CommandQueue commandBuffer];
         id<MTLRenderCommandEncoder> RenderCommandEncoder = [CommandBuffer
-            renderCommandEncoderWithDescriptor:RenderPassDescriptor];
+                                                            renderCommandEncoderWithDescriptor:RenderPassDescriptor];
         [RenderCommandEncoder setRenderPipelineState:RenderPipelineState];
         [RenderCommandEncoder setVertexBuffer:VertexBuffer offset:0 atIndex:0];
-
+        
         [RenderCommandEncoder setFragmentBuffer:SphereBuffer
-                                         offset:0
-                                        atIndex:0];
-
+         offset:0
+         atIndex:0];
+        
         [RenderCommandEncoder setFragmentBuffer:SphereCountBuffer
-                                         offset:0
-                                        atIndex:1];
-
+         offset:0
+         atIndex:1];
+        
         [RenderCommandEncoder setFragmentBuffer:CameraBuffer
-                                         offset:0
-                                        atIndex:2];
-
+         offset:0
+         atIndex:2];
+        
         i32 GridSize   = 1;
         i32 TotalTiles = GridSize * GridSize;
         f64 TileWidth  = IMAGE_WIDTH / GridSize;
@@ -510,7 +510,7 @@ main(i32 argc, const char *argv[])
         {
             for(i32 Col = 0; Col < GridSize; ++Col)
             {
-
+                
                 f64         OriginX = Col * TileWidth;
                 f64         OriginY = Row * TileHeight;
                 MTLViewport Viewport;
@@ -522,31 +522,31 @@ main(i32 argc, const char *argv[])
                 Viewport.zfar    = 1.0;
                 [RenderCommandEncoder setViewport:Viewport];
                 [RenderCommandEncoder drawPrimitives:MTLPrimitiveTypeTriangle
-                                         vertexStart:0
-                                         vertexCount:6];
+                 vertexStart:0
+                 vertexCount:6];
             }
         }
-
+        
         [RenderCommandEncoder endEncoding];
         [CommandBuffer commit];
         [CommandBuffer waitUntilCompleted];
-
+        
         u8 *Pixels = (u8 *)malloc(IMAGE_HEIGHT * (IMAGE_WIDTH * 4));
-
+        
         [Texture getBytes:Pixels
-              bytesPerRow:IMAGE_WIDTH * 4
-               fromRegion:MTLRegionMake2D(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT)
-              mipmapLevel:0];
-
+         bytesPerRow:IMAGE_WIDTH * 4
+         fromRegion:MTLRegionMake2D(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT)
+         mipmapLevel:0];
+        
         char OutputFilename[] = "output.ppm";
         char OutputFullPath[PATH_MAX];
-
+        
         BuildFullPath(&State, OutputFilename, sizeof(OutputFilename),
                       OutputFullPath);
-
+        
         FILE *File = fopen(OutputFullPath, "wb");
         fprintf(File, "P6\n%d %d\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
-
+        
         for(int Row = 0; Row < IMAGE_HEIGHT; ++Row)
         {
             for(int Col = 0; Col < IMAGE_WIDTH; ++Col)
@@ -555,9 +555,9 @@ main(i32 argc, const char *argv[])
                 fwrite(Pixel, 1, 3, File);
             }
         }
-
+        
         fclose(File);
-
+        
         printf("ray tracing done\n");
     }
 }
